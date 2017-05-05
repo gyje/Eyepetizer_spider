1eeb819d1327fbfcccaf7d16d5419e1e85682f0e
YingJie <gyj37191@gmail.com>
2017-05-05 14:17:49 +0800

    Update Eyepetizer_spider.py

--- Eyepetizer_spider.py
+++ Eyepetizer_spider.py
@@ -6,7 +6,8 @@ date=time.strftime("%Y%M%D",time.localtime(time.time()))
 url="http://baobab.kaiyanapp.com/api/v1/feed?date="+date
 json_text=requests.get(url,headers=headers).text
 json_dict=json.loads(json_text)
-fob=open("1.txt","w+")
+s=time.clock()
+fob=open("0.txt","w+")
 video_num=int(json_dict["dailyList"][0]["total"])
 all_video_list=json_dict["dailyList"][0]["videoList"]
 for i in range(video_num):
@@ -18,3 +19,8 @@ for i in range(video_num):
 	fob.write("播放地址(七牛云):"+all_video_list[i]["playUrl"].strip("ucloud")+"qcloud"+"\n")
 	fob.write("视频时长:"+str(all_video_list[i]["duration"])+"秒\n\n\n")
 fob.close()
+e=time.clock()
+print ("{}S".format(e-s))
+'''
+说明:尝试了用字典和列表的形式减少磁盘I/O,在2天的数据获取测试中,虽然时间差距较大(39倍),但是整体花费时间并没有比较大的影响,这种方式时间为:0.0396258597025203秒,而用字典存储时间为:0.0014271225123327833秒,而且字典是无序的还需要更多的代码实现排序,所以,还是选择这种方式.
+'''
